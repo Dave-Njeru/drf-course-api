cf9a086d4d2fae4fac4420042b8fbd32473c0803
Dave-Njeru <davenjeru9@gmail.com>
2026-05-30 05:47:54 +0300

    feat: create view for product serializer

--- drf_course/api/views.py
+++ drf_course/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from api.serializers import ProductSerializer
+from api.models import Product
+from django.http import JsonResponse
+# from rest_framework.response import Response
+# from rest_framework.renderers import JSONRenderer
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return JsonResponse({
+        'data': serializer.data
+    })
