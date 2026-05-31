d6db7f438e150dd470cb530f5d7d4b23300a1830
Dave-Njeru <davenjeru9@gmail.com>
2026-05-31 12:45:50 +0300

    feat: serializers method field
    - And further nested relationships

--- drf_course/api/serializers.py
+++ drf_course/api/serializers.py
@@ -12,13 +12,31 @@ class ProductSerializer(serializers.ModelSerializer):
         return value
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(source='product.price', max_digits=10, decimal_places=2)
+
     class Meta:
         model = OrderItem
-        fields = ('product', 'quantity')
+        fields = (
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal',
+        )
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        orders = obj.items.all()
+        # return sum(order_item.item_subtotal for order_item in orders) == Generator expression to calculate total price of the order
+
+        total = 0
+        for order_item in orders:
+            total += order_item.item_subtotal
+        return total
 
     class Meta:
         model = Order
-        fields = ('order_id', 'created_at', 'user', 'status', 'items')
+        fields = ('order_id', 'created_at', 'user', 'status', 'items', 'total_price')
