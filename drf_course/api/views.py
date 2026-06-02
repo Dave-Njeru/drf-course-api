d98e892d743b246ba894c81c18efa8cd2269bc41
Dave-Njeru <davenjeru9@gmail.com>
2026-06-02 20:40:02 +0300

    feat: generic views
    - Changed FBVs to CBVs

--- drf_course/api/views.py
+++ drf_course/api/views.py
@@ -4,24 +4,19 @@ from api.models import Product, Order
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0)  # Only return products that are in stock
+    serializer_class = ProductSerializer
 
-@api_view(['GET'])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-@api_view(['GET'])
-def order_list(request):
-    orders = Order.objects.prefetch_related('items__product')  # Optimize query to fetch related products
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')  # Optimize query to fetch related products
+    serializer_class = OrderSerializer
 
 # Aggregate API data for products info
 @api_view(['GET'])
